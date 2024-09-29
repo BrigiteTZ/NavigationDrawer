d2bdac56b301d7c697db641b230c11bce8534d7e
BrigiteTZ <btarazonazambrano@gmail.com>
2024-09-29 12:48:51 -0500

    Gallery Fragment con la web view

--- app/src/main/java/dev/luischang/navigationdrawer/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/luischang/navigationdrawer/ui/gallery/GalleryFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import dev.luischang.navigationdrawer.databinding.FragmentGalleryBinding
+import dev.luischang.navigationdrawer.ui.shared.HomeGalleryViewModel
 
 class GalleryFragment : Fragment() {
 
@@ -16,22 +21,33 @@ class GalleryFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val homeGalleryViewModel:HomeGalleryViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
-
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val wvPage:WebView=binding.wvPage
+        wvPage.settings.javaScriptEnabled=true
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        wvPage.webViewClient=object:WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                url:String
+            ): Boolean {
+                if (url !=null)
+                view?.loadUrl(url)
+                return true
+            }
         }
+
+        homeGalleryViewModel.url.observe(viewLifecycleOwner){url ->
+            wvPage.loadUrl(url)
+        }
+
         return root
     }
 
